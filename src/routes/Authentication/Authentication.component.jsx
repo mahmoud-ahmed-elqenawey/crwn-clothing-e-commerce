175ddbd02cbccc0de5c0e69ddf9360dec4a87d5c
QeNo <moodiiy.98@gmail.com>
2022-04-01 14:41:23 +0200

    responsive auth

--- src/routes/Authentication/Authentication.component.jsx
+++ src/routes/Authentication/Authentication.component.jsx
@@ -1,13 +1,20 @@
 import SignUpForm from '../../components/SignUpForm/SignUpForm.component';
 import SignInForm from '../../components/SignInForm/SignInForm.component';
 import './Authentication.styles.scss';
+import { Container, Grid } from '@mui/material';
 
 const Authentication = () => {
     return (
-        <div className='authentication-container'>
-            <SignInForm />
-            <SignUpForm />
-        </div>
+        <Container>
+            <Grid container alignItems='center' justifyContent='center'>
+                <Grid item xs={12} md={6}>
+                    <SignInForm />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <SignUpForm />
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
